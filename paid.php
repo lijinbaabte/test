da7ec581f159eb309a457c14f7c1b943f6cbea15
Lijin <lijin@baabte.com>
2015-01-12 13:22:26 +0530

    changed

--- paid.php
+++ paid.php
@@ -1,5 +1,8 @@
 <?php
 
-echo "hai";
+$con=mysqli_connect("localhost","root","baabtra","lijintest");
+$req= implode(" - ",$_REQUEST);
+$result=mysqli_query($con,"insert into tbl_test(vchr_msg)values('$req')");
+
 
 ?>
